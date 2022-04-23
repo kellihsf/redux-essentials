7349c28cb19b5de0356663ff2d423ab29ba4a480
Kelli <kellistone217@gmail.com>
2022-04-23 08:13:21 -0600

    Installed initial files from tutorial

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
-  reducer: {
+  reducer: { // each key should match an action
     counter: counterReducer,
   },
 });
